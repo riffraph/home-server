088b083ad72d2240b2de75e1fc31654f82b705a0
Raphael Chan <raphaelc@gmail.com>
2022-06-06 23:05:03 +0200

    added script to initialise the server

---
+++ media-apps/init-server.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+# create docker network
+
+docker network create \
+-d macvlan \
+--subnet=192.168.86.0/24 \
+--gateway=192.168.86.1 \
+--ip-range=192.168.86.30/28 \
+-o parent=eth0 \
+docker-network
+
+
+# create groups
+
+groupadd -g 1002 download
+
+
+# create users
+
+useradd -U nzbget -G 1002
+useradd -U sonarr -G 1002
+useradd -U plex -G 1002
+
+
+# create folders
+
+mkdir /downloads
+mkdir -p /media/movies
+mkdir -p /media/tv
+mkdir -p /serverapps/nzbget/config
+mkdir -p /serverapps/sonarr/config
+mkdir -p /serverapps/plex/config
+mkdir -p /serverapps/plex/transcode
